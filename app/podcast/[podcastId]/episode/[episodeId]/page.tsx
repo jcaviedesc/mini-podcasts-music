35d3a575499b7b75da080d516961be9da737a108
jcaviedesc <jcamilocc97@gmail.com>
2023-07-14 01:50:49 +0200

    feat(episode): create episode reproductor

--- app/podcast/[podcastId]/episode/[episodeId]/page.tsx
+++ app/podcast/[podcastId]/episode/[episodeId]/page.tsx
@@ -8,5 +8,24 @@ type pageProps = {
 
 export default function EpisodeDetailPage({}: pageProps) {
   const { episode } = useEpisode();
-  return <h1>Hello, episode page</h1>;
+  return (
+    <div className="bg-white rounded-lg p-6 shadow">
+      <div>
+        <h1 className="text-gray-900 font-bold text-lg">
+          {episode?.trackName}
+        </h1>
+      </div>
+      <p
+        className="text-gray-900 mt-2 mb-6"
+        dangerouslySetInnerHTML={{ __html: episode?.description ?? "" }}
+      />
+      <audio controls className="w-full">
+        <source
+          src={episode?.episodeUrl}
+          type={`${episode?.episodeContentType}/${episode?.episodeFileExtension}`}
+        />
+        Tu navegador no soporta el elemento de audio.
+      </audio>
+    </div>
+  );
 }
